036331ce643878dd37c9c168f17b52839b5dd43d
Kanik Goyal <acrotechllp@gmail.com>
2021-08-29 00:10:16 +0530

    howcheck

--- src/Components/Cartc.svelte
+++ src/Components/Cartc.svelte
@@ -18,6 +18,21 @@
 .zz{
     color: cornflowerblue;
 }
+.jad{
+  display: flex;
+  justify-content: space-between;
+
+}
+.mad{
+  
+  align-self: center;
+  margin-right: 5px;
+  
+}
+.tad{
+  align-self: center;
+}
+
 
 </style>
 
@@ -25,13 +40,28 @@
 <ul class="w3-ul w3-card-4 bb">
 {#each tain as item}
 <li class="w3-bar">
-  <span onclick="this.parentElement.style.display='none'"
-  class="w3-bar-item w3-button w3-xlarge w3-right">&times;</span>
+  <div class="jad">
   <img src="{item.Images}" class="w3-bar-item w3-circle aa" alt="products">
   <div class="w3-bar-item">
     <span class="w3-large">{item.Title}</span><br>
-    <span class="zz">₹{item.Price}</span>
+    <span class="zz">₹{item.Price} × {item.Quantity}</span>
   </div>
+  <div class="tad">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg><br>
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-square" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+  <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
+</svg>
+</div>
+<div class="mad">
+  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+</svg>
+</div>
+</div>
 </li>
 {/each}
 </ul>
